a7e63af72a049d2ef3f6b8dfa0b065733acc4212
mochimisu <brandonwang@me.com>
2025-02-04 02:08:36 -0800

    a

--- src/main.ts
+++ src/main.ts
@@ -73,6 +73,8 @@ declare global {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
+  console.log(`Current game tick is ${Game.time}`);
+
   if (!harvesterNoMoveSpawnLoop()) {
     spawnLoop();
     muleSpawnLoop();
